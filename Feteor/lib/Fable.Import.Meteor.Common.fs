0f0ec727ae3d9b9aca1c1c89e28c2e76be9500b7
Roman Melnikov <oildollar@gmail.com>
2017-01-02 02:25:31 +0300

    fixing Types

--- Feteor/lib/Fable.Import.Meteor.Common.fs
+++ Feteor/lib/Fable.Import.Meteor.Common.fs
@@ -6,6 +6,20 @@ open Fable.Import.JS
 
 
 
+[<KeyValueList>]
+type Methods = 
+    |[<Erase>] Method of methodname:string * Function
+
+[<KeyValueList>]
+type ApplyOptions = 
+    ///(Client only) If true, don't send this method until all previous method calls have completed, and don't send any subsequent method calls until this one is completed.
+    | Wait of bool
+    ///(Client only) This callback is invoked with the error or result of the method (just like asyncCallback) as soon as the error or result is available. The local cache may not yet reflect the writes performed by the method.
+    | OnResultReceived of Function
+    ///(Client only) if true, don't send this method again on reload, simply call the callback an error with the error code 'invocation-failed'.
+    | NoRetry of bool
+
+
 type SettingsType() =
         //abstract ``public``: obj with get, set
         [<Emit("$0[$1]{{=$2}}")>]
@@ -16,15 +30,14 @@ type SettingsType() =
 ///     secure Boolean Create an HTTPS URL.
 ///     replaceLocalhost Boolean  Replace localhost with 127.0.0.1. Useful for services that don't recognize localhost as a domain name.
 ///     rootUrl String   Override the default ROOT_URL from the server environment. For example: "http://foo.example.com"
-type absoluteUrlOptions = {
-
+[<KeyValueList>]
+type AbsoluteUrlOptions =
     ///  Create an HTTPS URL.
-    mutable secure :bool
+    | Secure
     ///  Replace localhost with 127.0.0.1. Useful for services that don't recognize localhost as a domain name.
-    mutable replaceLocalhost :bool
-    ///  Override the default ROOT_URL from the server environment. For example: "http://foo.example.com"  
-    mutable rootUrl :string
-}
+    | ReplaceLocalhost
+    ///Override the default ROOT_URL from the server environment. For example: "http://foo.example.com"  
+    | RootUrl of string
 
 type [<Import("Meteor","meteor/meteor")>] Meteor =
     ///Boolean variable. True if running in client environment.
@@ -89,7 +102,7 @@ type [<Import("Meteor","meteor/meteor")>] Meteor =
     ///  <param name="options">
     ///     <see cref="absoluteUrlOptions"/>
     /// </param>
-    static member absoluteUrl(?path: string, ?options: absoluteUrlOptions): string = jsNative
+    static member absoluteUrl(?path: string, ?options: AbsoluteUrlOptions list): string = jsNative
     /// <summary>
     ///     Meteor.settings contains deployment-specific configuration options. You can initialize settings by passing the --settings option (which takes the name of a file containing JSON data) to meteor run or meteor deploy. When running your server directly (e.g. from a bundle), you instead specify settings by putting the JSON directly into the METEOR_SETTINGS environment variable. If the settings object contains a key named public, then Meteor.settings.public will be available on the client as well as the server. All other properties of Meteor.settings are only defined on the server. You can rely on Meteor.settings and Meteor.settings.public being defined objects (not undefined) on both client and server even if there are no settings specified. Changes to Meteor.settings.public at runtime will be picked up by new client connections.
     /// </summary>
@@ -97,5 +110,72 @@ type [<Import("Meteor","meteor/meteor")>] Meteor =
     /// <summary>
     ///     Defines functions that can be invoked over the network by clients.
     /// </summary>
-    static member methods(methods: obj): unit = jsNative
+    static member methods(methods: Methods list): unit = jsNative
     
+    ///<summary>
+    ///Invokes a method passing any number of arguments.
+    ///</summary>
+    ///<param name="name">
+    ///Name of method to invoke
+    ///</param>
+    ///<param name="args">
+    ///Optional method arguments with optional callback, which is called asynchronously with the error or result after the method is complete. If not provided, the method runs synchronously if possible
+    ///</param>
+    static member call(name:string, [<ParamArray>] args: obj[]):Option<'T> = jsNative
+    ///<summary>
+    ///</summary>
+    ///<param name="name">
+    ///Name of method to invoke
+    ///</param>
+    ///<param name="args"
+    /// Method arguments
+    ///</param>
+    ///<param name="options">
+    /// wait Boolean
+    /// (Client only) If true, don't send this method until all previous method calls have completed, and don't send any subsequent method calls until this one is completed.
+    /// onResultReceived Function
+    /// (Client only) This callback is invoked with the error or result of the method (just like asyncCallback) as soon as the error or result is available. The local cache may not yet reflect the writes performed by the method.
+    /// noRetry Boolean
+    /// (Client only) if true, don't send this method again on reload, simply call the callback an error with the error code 'invocation-failed'.
+    ///</param>
+    static member apply(name:string, ?args:obj[], ?options:ApplyOptions list, ?callback:Function):Option<'T> = jsNative
+type ObjectID = 
+    [<Emit("new $0($1)")>]
+    static member Create(hexString:string):ObjectID = jsNative
+
+
+// [<KeyValueList>]
+// type MongoSelector =
+//     | [<CompiledName("$or")>]OR of MongoSelector list
+//     | [<Erase>] Field of U2<obj,MongoSelector list>
+[<KeyValueList>]    
+type CollectionFindOptions = 
+    | Sort of (string * int) list
+    | Skip of int
+    | Limit of int
+    | Reactive of bool
+    | Transform of Function
+    | DisableOplog of bool
+    | PollingIntervalMs of int
+    | PollingThrottleMs of int
+    | Fields of (string * int) list
+
+type Collection<'T> = 
+    abstract find : ?selector:U3<ObjectID, String, obj> * ?options:CollectionFindOptions list = jsNative
+    
+
+[<StringEnum>]
+type IdGenerationType =
+    | [<CompiledName("STRING")>]STRING
+    | [<CompiledName("MONGO")>]MONGO
+
+[<KeyValueList>]
+type MongoOptions = 
+    | Connection of obj
+    | IdGeneration of IdGenerationType
+    | Transform of Function
+    | DefineMutationMethods of bool
+
+type [<Import("Mongo","meteor/mongo")>] Mongo =
+    static member Collection(?name:string, ?options:MongoOptions list):Collection<'T> = jsNative
+
